218555e2208e57806c782158b19aee222d828b9a
Ahmad Hassaan <ahmedoppal2592@gmail.com>
2025-03-20 10:21:36 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import PureComp from './Components/PureComp'
+import MemoComp from './Components/MemoComp'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      name:"Ahmad"
+    }
+  }
+  render() {
+    return (
+      <div>
+        <PureComp />
+        <MemoComp name={this.state.name}/>
+      </div>
+    )
+  }
 }
-
-export default App;
