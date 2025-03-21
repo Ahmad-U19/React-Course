8e5381fc6cd70a9b01ffe28e0f3757717490dc43
Ahmad Hassaan <ahmedoppal2592@gmail.com>
2025-03-21 10:35:49 -0700

    Refs Practice

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react'
-import PureComp from './Components/PureComp'
-import MemoComp from './Components/MemoComp'
+import RefsDemo from './Components/RefsDemo'
+//import PureComp from './Components/PureComp'
+//import MemoComp from './Components/MemoComp'
 
 export default class App extends Component {
-  constructor(){
-    super();
-
-    this.state = {
-      name:"Ahmad"
-    }
-  }
   render() {
     return (
       <div>
-        <PureComp />
-        <MemoComp name={this.state.name}/>
+        {/* <PureComp />
+        <MemoComp name={this.state.name}/> */}
+        <RefsDemo />
       </div>
     )
   }
